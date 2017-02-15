cfb996d41069400a63390dfea376e9b7e1cbebfa
tolland1 <jaan.tollander@gmail.com>
2017-02-15 12:36:00 +0200

    test create particles equal

--- package/tests/test_jitclass.py
+++ package/tests/test_jitclass.py
@@ -4,6 +4,7 @@ Attributes:
     SIZE: Number of particles
 """
 import random
+import numpy as np
 
 from package.jitclass import Particles, create_particles_numpy, \
     create_particles_jitclass
@@ -11,9 +12,9 @@ from package.jitclass import Particles, create_particles_numpy, \
 SIZE = int(1000)
 
 
-def test_():
+def test_create_particles():
     # Random seed so that both particle create function create same values.
-    seed = random.randint()
+    seed = random.randint(0, 100)
 
     # Custom Numpy dtype
     particles = create_particles_numpy(SIZE, seed)
@@ -23,7 +24,10 @@ def test_():
     create_particles_jitclass(particles2, seed)
 
     # Test that all created values are the same
-    assert True
+    assert np.allclose(particles['x'], particles2.x)
+    assert np.allclose(particles['y'], particles2.y)
+    assert np.allclose(particles['z'], particles2.z)
+    assert np.allclose(particles['m'], particles2.m)
 
 
 def test_potential():
